af8023348181d1e69f901f34695be9c37a79bcc5
saeed <saeed@mary.local>
2023-05-20 21:34:02 +0330

    first commit and also completing Hero component

---
+++ src/assets/index.js
@@ -0,0 +1,7 @@
+import linkIcon from "./link.png";
+import loader from "./loader.png";
+import copy from "./copy.png";
+import logo from "./logo.png";
+import tick from "./tick.png";
+
+export {linkIcon, loader, copy, logo, tick};
---
+++ src/component/Hero.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {logo} from "../assets";
+
+const Hero = () => {
+  return (
+    <header className="w-full flex flex-col justify-center items-center">
+      <nav className="flex flex-row w-full justify-between items-center mb-10 pt-3">
+        <img src={logo} alt="logo" className="w-28 object-contain" />
+        <button
+          type="button"
+          onClick={() => {
+            // window.open();
+          }}
+          className="black_btn">
+          GitHub
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden" />
+        <span className="orange_gradient bg-gradient-to-r from-amber-500 via-orange-600 to-yellow-500">
+          OpenAI GPT-4{" "}
+        </span>
+      </h1>
+
+      <h2 className="desc">
+        Simplify your reading with Summize, an open-source article summarizer
+        that transforms lengthy articles into clear and concise summaries
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
